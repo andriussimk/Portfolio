2c67c73322aee113b4c8d22f4fbb08f14f4df8d8
andriussimk <andrsimkus@gmail.com>
2025-12-19 02:10:59 +0200

    Updated contact info

--- src/pages/contacts.ts
+++ src/pages/contacts.ts
@@ -3,8 +3,8 @@ export function renderContactsPage() {
         <h1>Contact Me</h1>
         <p>If you would like to get in touch, feel free to reach out through any of the following methods:</p>
         <ul>
-            <li>Email: <a href="mailto:your-email@example.com">your-email@example.com</a></li>
-            <li>Phone: <a href="tel:+1234567890">+1 (234) 567-890</a></li>
+            <li>Email: <a href="mailto:hello@shotbyandrius.com">hello@shotbyandrius.com</a></li>
+            <li>Phone: <a href="tel:+37061804969">+37061804969</a></li>
         </ul>
         <h2>Follow Me</h2>
         <ul>
